528ca9c3d3934d6a12590ef903380ff3b7ad5f3f
JosephPrichard <Ltprichard2019@gmail.com>
2023-10-24 00:29:41 -0500

    Added rough draft for a SHM cleanup algorithm

---
+++ handler/src/manager.cpp
@@ -0,0 +1,81 @@
+#include <syslog.h>
+#include "manager.h"
+
+FileManager::FileManager(off_t memory_size) {
+    this->memory_size = memory_size;
+    auto fb = FreeBlock {
+        .left_offset = 0,
+        .right_offset = memory_size
+    };
+    free_blocks.emplace_back(fb);
+}
+
+FileMetadata FileManager::reserve_file(const std::string &name, off_t size) {
+    if (files.count(name) > 0) {
+        syslog(LOG_ERR, "Not enough shared memory to allocate a file to: no FreeBlocks");
+    }
+
+    // search for the first FreeBlock that can fit the file we want to allocate
+    FreeBlock* fb = nullptr;
+    for (auto &curr_fb : free_blocks) {
+        if (curr_fb.right_offset - curr_fb.left_offset > size) {
+            fb = &curr_fb;
+            break;
+        }
+    }
+    if (fb == nullptr) {
+        syslog(LOG_ERR, "Not enough shared memory to allocate a file to: cannot find FreeBlock large enough to fit the file");
+    }
+
+    auto md = FileMetadata{
+        .offset = fb->left_offset,
+        .size = size
+    };
+    name.copy(md.name, MD_STR_SIZE);
+    files[name] = md;
+
+    fb->left_offset += size;
+
+    // remove a fb if it no longer has any space left
+    if(fb->right_offset - fb->left_offset == 0) {
+        free_blocks.erase(free_blocks.begin());
+    }
+}
+
+void FileManager::free_file(const std::string &name) {
+    auto md = files[name];
+    files.erase(name);
+
+    auto new_fb = FreeBlock {
+        .left_offset = md.offset,
+        .right_offset = md.offset + md.size
+    };
+
+    // if there are no free blocks at all, we just create a new one and stop
+    if (free_blocks.empty()) {
+        free_blocks.emplace_back(new_fb);
+        return;
+    }
+
+    auto fb = free_blocks.begin();
+    for (; fb != free_blocks.end(); fb++) {
+        if (fb->left_offset == new_fb.right_offset) {
+            // found a free block to the right that can merged with the new block (it starts where the new one ends)
+            fb->left_offset -= md.size;
+            break;
+        } else if (fb->left_offset > new_fb.right_offset) {
+            // found the block the new block must be before (but there is space between them)
+            free_blocks.insert(fb, new_fb);
+            break;
+        }
+    }
+    // attempt to merge the free block with the one to the left if it can be done
+    if (fb != free_blocks.begin()) {
+        auto left_fb = fb - 1;
+        if (fb->left_offset == left_fb->right_offset) {
+            // merge the left free block to this one
+            fb->left_offset -= left_fb->right_offset - left_fb->left_offset;
+            free_blocks.erase(left_fb);
+        }
+    }
+}
---
+++ handler/src/manager.h
@@ -0,0 +1,29 @@
+
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include "metadata.h"
+
+struct FreeBlock {
+    off_t left_offset;
+    off_t right_offset;
+} typedef FreeBlock;
+
+// keeps track of the named files stored in another block of memory such as a shared memory
+// doesn't actually write to the block of memory that this is used to keep track of, rather gives offsets used to calculate pointers
+class FileManager {
+private:
+    off_t memory_size; // size of the memory the files are in
+    std::unordered_map<std::string, FileMetadata> files; // the files currently in the memory
+    std::vector<FreeBlock> free_blocks; // data structure to store the free_file space to reserve_file efficiently
+public:
+    explicit FileManager(off_t);
+
+    // reserves an offset and size for a named object within the block of memory
+    // actual writing to the offset and size must happen elsewhere
+    FileMetadata reserve_file(const std::string &name, off_t size);
+
+    // frees a region used for a named file by releasing free_file space and reference to the metadata
+    // the freed region will NOT be zeroed out, but the offset range may appear in a subsequent reserve_file call
+    void free_file(const std::string &name);
+};
